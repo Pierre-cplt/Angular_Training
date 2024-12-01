f9faa03d38dfde72692e4069c1bd11b62d0cd487
CREPLPI <pierrecreplet@hotmail.com>
2024-12-01 12:19:45 +0100

    Services - 2

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -21,11 +21,7 @@ export class TasksComponent {
   get selectedUserTasks() {
     return this.tasksService.getUserTasks(this.userId);
   }
-
-  onCompleteTask(id: string){
-    this.tasksService.removeTask(id);
-  }
-
+  
   onStartAddTask(){
     this.isAddingTask = true;
   }
