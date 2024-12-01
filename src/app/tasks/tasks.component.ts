53a9f42b5b4f3de98a2e47ac716bc2a92b20fc3d
CREPLPI <pierrecreplet@hotmail.com>
2024-12-01 11:32:51 +0100

    FORMS

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,15 +1,18 @@
 import { Component, Input, Output } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
+import { NewTaskComponent } from "./new-task/new-task.component";
+import { NewTaskData } from './task/task.model';
 @Component({
   selector: 'app-tasks',
   standalone: true,
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
-  imports: [TaskComponent]
+  imports: [TaskComponent, NewTaskComponent]
 })
 export class TasksComponent {
   @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
+  isAddingTask = false; //pas besoin de définir le type car TS va déterminer automatiquement que c'est un boolean
   tasks = [
     {
       id: 't1',
@@ -42,6 +45,25 @@ export class TasksComponent {
 
   onCompleteTask(id: string){
     this.tasks = this.tasks.filter((task) => task.id != id);
+  }
+
+  onStartAddTask(){
+    this.isAddingTask = true;
+  }
+
+  onCancelAddTask() {
+    this.isAddingTask = false;
+  }
+
+  OnAddTask(taskData: NewTaskData){
+    this.tasks.push({
+      id: new Date().getTime().toString(),
+      userId: this.userId,
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.date,
+    });
+    this.isAddingTask = false;
   }
 
 }
